ba2f61b3f8155807875f82734e395a06fc7d39e2
Darek <zaik_d@onet.eu>
2018-07-03 14:23:07 +0200

    Dodano calkiem nowe funkcje

--- Zadanie1/src/Zadanie1.java
+++ Zadanie1/src/Zadanie1.java
@@ -3,6 +3,8 @@ public class Zadanie1 {
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
+        System.out.println('i');
+        System.out.println("commitowania");
     }
     }
 
