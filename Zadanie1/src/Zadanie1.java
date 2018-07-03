7939f9b0905ec47e6d0a4df126245010397b8122
Darek <zaik_d@onet.eu>
2018-07-03 14:19:04 +0200

    Dodano nowe funkcje

---
+++ Zadanie1/src/Zadanie1.java
@@ -0,0 +1,8 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+    }
+    }
+
